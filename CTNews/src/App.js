16a74cf48cdfde743c5d33fd4283e6f49c2ab9be
ThanhNhan226 <vuthanhnhan950@gmail.com>
2022-03-04 13:22:07 +0700

    update

--- CTNews/src/App.js
+++ CTNews/src/App.js
@@ -2,11 +2,13 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Trangchu from "./pages/Trangchu";
+import Dashboard from "./components/Dashboard";
+import Preferences from "./components/Preferences";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" className="wrapper">
       <Router>
         <Navbar />
         <Switch>
@@ -15,6 +17,16 @@ function App() {
         </Switch>
         <Footer />
       </Router>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+          <Route path="/preferences">
+            <Preferences />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
